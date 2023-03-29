4baaeb4c6ef3728380ba0ca5270241731b2f4817
glados1235 <Glados12351235@gmail.com>
2023-03-28 20:15:43 -0700

    attackanims go brrrrrrrrrr

--- src/main/java/net/tombvali/topatomod/entities/mobs/LostOneEntity.java
+++ src/main/java/net/tombvali/topatomod/entities/mobs/LostOneEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
